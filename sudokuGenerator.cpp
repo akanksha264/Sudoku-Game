3a2118be600f76b399e80cdf352a76c5ba9e3b01
akanksha264 <85816105+akanksha264@users.noreply.github.com>
2022-08-02 12:06:21 +0530

    Create sudokuGenerator.cpp
    Generate a sudoku to be solved

---
+++ sudokuGenerator.cpp
@@ -0,0 +1,207 @@
+/*
+GENERATE FINAL SUDOKU FOR USER TO SOLVE
+BY - AKANKSHA KEJRIWAL
+DATE - 24th JUNE 2022
+*/
+
+#include <iostream>
+#include <vector>
+#include <conio.h>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+
+class Sudoku
+{
+    vector<vector<int>> board;
+    vector<vector<int>> state;  //State of board while deleting cells
+    vector<vector<int>> solution;   //Store solution
+    int n;
+
+    // Private member functions
+    bool fillBoard();
+    bool solve(int, int);
+    bool isValid(int, int, int);
+    void deleteCells();
+
+public:
+    Sudoku()
+    {
+        n = 9;
+        vector<int> row(n, 0);
+        for (int i = 0; i < n; i++)
+            board.push_back(row);
+
+        state=board;
+        
+        //Set seed for random number generator
+        srand(time(0));
+
+        //Generate a board until a possible configuration is found
+        bool isGenerated=false;
+        do
+        {
+            isGenerated=fillBoard();
+        } while (!isGenerated);
+
+        deleteCells();
+    }
+
+    void display();
+};
+
+bool Sudoku::fillBoard()
+{
+    //Filling diagonal 3x3 boxes with random number from 1 to 9
+    for(int i=0 ; i<n ; i+=3)
+    {
+        bool used[9]={0}; //Keep a record of filled numbers
+
+        for(int x=0 ; x<3 ; x++)
+        {
+            for(int y=0 ; y<3 ; y++)
+            {
+                int num;
+                do
+                {
+                    //Generate a random number
+                    num=rand()%9 + 1;
+                } while (used[num-1]); //Loop until an unused number is not found
+                
+                //Mark number as used
+                used[num-1]=1;
+                //Fill number in cell
+                board[x+i][y+i]=num;
+            }
+        }
+    }
+
+    //Now solve the sudoku and check if a solution is possible
+    return solve(0,0);
+}
+
+bool Sudoku::solve(int r, int c)
+{
+    // If last row is complete sudoku is solved
+    if (r >= 9)
+        return true;
+
+    // If column end is reached, change row
+    if (c >= 9)
+        return solve(r + 1, 0);
+
+    // If cell is not empty, go to next cell
+    if (board[r][c] != 0)
+        return solve(r, c + 1);
+
+    // Fill the cell with a number
+    for (int i = 1; i <= 9; i++)
+    {
+        if (isValid(r, c, i))
+        {
+            board[r][c] = i;
+            if (solve(r, c + 1))
+                return true;
+            board[r][c] = 0;
+        }
+    }
+
+    return false;
+}
+
+bool Sudoku::isValid(int row, int col, int val)
+{
+    // Checking the row
+    for (int i = 0; i < n; i++)
+        if (board[row][i] == val)
+            return false;
+
+    // Checking the column
+    for (int i = 0; i < n; i++)
+        if (board[i][col] == val)
+            return false;
+
+    // Checking the 3x3 box
+    int i = (row / 3) * 3;
+    int j = (col / 3) * 3;
+    for (int x = 0; x < 3; x++)
+        for (int y = 0; y < 3; y++)
+            if (board[i + x][j + y] == val)
+                return false;
+
+    return true;
+}
+
+void Sudoku::deleteCells()
+{
+    //We will remove maximum 45 cells
+
+    //Set board as solution
+    solution=board;
+
+    //Set initial state of all cells as 1(filled)
+    for(int i=0 ; i<n ; i++)
+        for(int j=0 ; j<n ; j++)
+            state[i][j]=1;
+
+    //State 0 is for empty cell
+    //State -1 is for a cell that cannot be removed
+    //int count=0;    //Removed cell count
+    for(int i=1 ; i<45 ; i++)
+    {
+        //Select a random cell
+        int cell=rand()%81;
+        int row=cell/9;
+        int col=cell%9;
+
+        //Check if cell is already deleted or cannot be deleted
+        if(state[row][col]==1)
+        {
+            //Remove the cell
+            state[row][col]=0;
+            board[row][col]=0;
+
+            //Check if multiple solutions are possible
+            //Fill current cell with numbers other than original number and run the solver
+            for(int j=1 ; j<=9 ; j++)
+            {
+                if(solution[row][col]==j)   continue;
+                if(isValid(row,col,j))
+                {
+                    board[row][col]=j;
+                    if(solve(0,0))
+                    {
+                        board[row][col]=solution[row][col];
+                        state[row][col]=-1;
+                        break;
+                    }
+                    board[row][col]=0;
+                }
+            }
+
+            //if(state[row][col]==0)
+            //    count++;
+        }
+    }
+    //cout << count << endl;
+}
+
+void Sudoku::display()
+{
+    cout << "\n\nRandom Sudoku :-\n";
+    cout << "---------------------\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << board[i][j] << " ";
+        cout << "\n";
+    }
+}
+
+main()
+{
+    Sudoku s;
+    s.display();
+
+    return 0;
+}
